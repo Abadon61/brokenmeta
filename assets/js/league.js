31a11fafe8e26834c057c4423f7029983b5cab96
Wuju <vincentduval61@gmail.com>
2026-09-20 00:21:35 +0200

    Deploy: new League landing page, grouped League menu, single-open dropdowns

--- assets/js/league.js
+++ assets/js/league.js
@@ -175,7 +175,7 @@
     if (bmChartsState) return;
     bmChartsState = 1;
     var sc = document.createElement('script');
-    sc.src = (window.BM_ROOT || '/') + 'assets/js/bm-charts.js?v=32b07f3edf';
+    sc.src = (window.BM_ROOT || '/') + 'assets/js/bm-charts.js?v=e20e5bb1b8';
     sc.async = true;
     document.head.appendChild(sc);
   }
