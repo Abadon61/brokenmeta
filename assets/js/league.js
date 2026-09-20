3fa72f507b62875487f10ff5ac739d6ee92d20a4
Wuju <vincentduval61@gmail.com>
2026-09-20 15:51:25 +0200

    Deploy: visible breadcrumb and BreadcrumbList on every League page

--- assets/js/league.js
+++ assets/js/league.js
@@ -175,7 +175,7 @@
     if (bmChartsState) return;
     bmChartsState = 1;
     var sc = document.createElement('script');
-    sc.src = (window.BM_ROOT || '/') + 'assets/js/bm-charts.js?v=a3f357699d';
+    sc.src = (window.BM_ROOT || '/') + 'assets/js/bm-charts.js?v=cd51149f36';
     sc.async = true;
     document.head.appendChild(sc);
   }
