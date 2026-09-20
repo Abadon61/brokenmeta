bb30a8cc1cb813b2ff54d9edaed5ae0e96a9e50b
Wuju <vincentduval61@gmail.com>
2026-09-20 17:39:06 +0200

    Deploy: League tier list cards and header artwork

--- assets/js/league.js
+++ assets/js/league.js
@@ -175,7 +175,7 @@
     if (bmChartsState) return;
     bmChartsState = 1;
     var sc = document.createElement('script');
-    sc.src = (window.BM_ROOT || '/') + 'assets/js/bm-charts.js?v=7f0277ea16';
+    sc.src = (window.BM_ROOT || '/') + 'assets/js/bm-charts.js?v=00e551c801';
     sc.async = true;
     document.head.appendChild(sc);
   }
