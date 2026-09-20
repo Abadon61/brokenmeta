b1e47362026aceb468e2496ee29557a7880d76ba
Wuju <vincentduval61@gmail.com>
2026-09-20 17:00:39 +0200

    Deploy: shared header on League detail pages

--- assets/js/league.js
+++ assets/js/league.js
@@ -175,7 +175,7 @@
     if (bmChartsState) return;
     bmChartsState = 1;
     var sc = document.createElement('script');
-    sc.src = (window.BM_ROOT || '/') + 'assets/js/bm-charts.js?v=cd51149f36';
+    sc.src = (window.BM_ROOT || '/') + 'assets/js/bm-charts.js?v=7f0277ea16';
     sc.async = true;
     document.head.appendChild(sc);
   }
