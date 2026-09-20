edc1b323517b351a9fd3541bdc85a7fbd697131c
Wuju <vincentduval61@gmail.com>
2026-09-20 23:26:38 +0200

    WoW: Classes et races page with class and race icons, Beta: Forever menu

--- assets/js/lol-compare.js
+++ assets/js/lol-compare.js
@@ -67,7 +67,7 @@
     if (bmChartsState) return;
     bmChartsState = 1;
     var sc = document.createElement('script');
-    sc.src = (window.BM_ROOT || '/') + 'assets/js/bm-charts.js?v=1ba6f7d5f3';
+    sc.src = (window.BM_ROOT || '/') + 'assets/js/bm-charts.js?v=f1735b506a';
     sc.async = true;
     document.head.appendChild(sc);
   }
