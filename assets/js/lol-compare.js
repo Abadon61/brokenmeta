e19157b18fc6e550490315b4cde4afb5154d02ce
Wuju <vincentduval61@gmail.com>
2026-09-20 22:39:49 +0200

    Deploy: WoW: Forever home redesign, 'Bêta : Forever' menu group, 'Calculateur de talents' entry

--- assets/js/lol-compare.js
+++ assets/js/lol-compare.js
@@ -67,7 +67,7 @@
     if (bmChartsState) return;
     bmChartsState = 1;
     var sc = document.createElement('script');
-    sc.src = (window.BM_ROOT || '/') + 'assets/js/bm-charts.js?v=29748ca6f9';
+    sc.src = (window.BM_ROOT || '/') + 'assets/js/bm-charts.js?v=1ba6f7d5f3';
     sc.async = true;
     document.head.appendChild(sc);
   }
