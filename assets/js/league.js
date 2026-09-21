0e7a80e29ec90ea982958ac9176769bb552da211
Wuju <vincentduval61@gmail.com>
2026-09-21 20:51:57 +0200

    WoW: guides for all 27 specializations (talent template, stat priority, consumables)

--- assets/js/league.js
+++ assets/js/league.js
@@ -175,7 +175,7 @@
     if (bmChartsState) return;
     bmChartsState = 1;
     var sc = document.createElement('script');
-    sc.src = (window.BM_ROOT || '/') + 'assets/js/bm-charts.js?v=afacc3433f';
+    sc.src = (window.BM_ROOT || '/') + 'assets/js/bm-charts.js?v=7c8b10a04f';
     sc.async = true;
     document.head.appendChild(sc);
   }
