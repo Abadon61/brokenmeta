f94d9831e5f34a27871b7fc32611195e5cd302de
Wuju <vincentduval61@gmail.com>
2026-09-23 11:42:55 +0200

    Sync: WoW character optimizer (paperdoll, race/class stats, trinkets)

--- assets/js/league.js
+++ assets/js/league.js
@@ -175,7 +175,7 @@
     if (bmChartsState) return;
     bmChartsState = 1;
     var sc = document.createElement('script');
-    sc.src = (window.BM_ROOT || '/') + 'assets/js/bm-charts.js?v=5ea9e62afa';
+    sc.src = (window.BM_ROOT || '/') + 'assets/js/bm-charts.js?v=cefb523953';
     sc.async = true;
     document.head.appendChild(sc);
   }
