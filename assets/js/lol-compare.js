33586151b5cd92bff2bd9a1849d1e001dc538e4c
Wuju <vincentduval61@gmail.com>
2026-09-23 02:37:55 +0200

    Sync: refreshed TFT data + Molten Core raid page

--- assets/js/lol-compare.js
+++ assets/js/lol-compare.js
@@ -67,7 +67,7 @@
     if (bmChartsState) return;
     bmChartsState = 1;
     var sc = document.createElement('script');
-    sc.src = (window.BM_ROOT || '/') + 'assets/js/bm-charts.js?v=50d6cc9919';
+    sc.src = (window.BM_ROOT || '/') + 'assets/js/bm-charts.js?v=5ea9e62afa';
     sc.async = true;
     document.head.appendChild(sc);
   }
